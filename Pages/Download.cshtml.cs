f5355fe3ad435e0a27dbdbb9fcb7bf441c1e3877
123 <123@DESKTOP-TOPEFL6>
2025-11-07 09:14:25 +0300

    Добавьте файлы проекта.

---
+++ Pages/Download.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TBSite.Pages
+{
+    public class DownloadModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
